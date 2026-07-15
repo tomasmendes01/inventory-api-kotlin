f54a3dd65eba857fae8434808f253833b380fd95
tomasmendes01 <imbladen88@gmail.com>
2026-07-15 22:27:51 +0100

    Dockerfile

---
+++ terraform/main.tf
@@ -0,0 +1,74 @@
+terraform {
+  required_providers {
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.32"
+    }
+  }
+}
+
+provider "kubernetes" {
+  config_path = "~/.kube/config"
+}
+
+resource "kubernetes_secret" "postgres_credentials" {
+  metadata {
+    name = "postgres-credentials"
+  }
+  data = {
+    POSTGRES_DB       = "inv-api-db"
+    POSTGRES_USER     = "inv-api-user"
+    POSTGRES_PASSWORD = "inv-api-secret"
+  }
+  type = "Opaque"
+}
+
+resource "kubernetes_deployment" "postgres" {
+  metadata {
+    name = "postgres"
+  }
+  spec {
+    replicas = 1
+    selector {
+      match_labels = {
+        app = "postgres"
+      }
+    }
+    template {
+      metadata {
+        labels = {
+          app = "postgres"
+        }
+      }
+      spec {
+        container {
+          name = "postgres"
+          image = "postgres:latest"
+          env_from {
+            secret_ref {
+              name = kubernetes_secret.postgres_credentials.metadata[0].name
+            }
+          }
+          port {
+            container_port = 5432
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_service" "postgres" {
+  metadata {
+    name = "postgres"
+  }
+  spec {
+    selector = {
+      app = "postgres"
+    }
+    port {
+      port = 5432
+      target_port = 5432
+    }
+  }
+}
